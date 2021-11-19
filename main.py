25d29f1fb45ebc27a31f513b7d33d136e4d9e278
LisandroFilloy <lisandrofilloy@gmail.com>
2021-11-18 22:10:23 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import telebot
+
+API_KEY = '2128358744:AAHk2RCqN89kR04RIWCrNgQob0sHn2LajNE'
+bot = telebot.TeleBot(API_KEY)
+gastos_licha = []
+gastos_juli = [10000]
+
+def deudor(gastosLicha, gastosJuli):
+    if gastosLicha < gastosJuli:
+        return "Licha"
+    else:
+        return "Juli"
+
+
+@bot.message_handler(commands=['Greet'])
+def greet(message):
+    bot.reply_to(message, "Hey, how's it going?")
+
+
+@bot.message_handler(commands=['Hello', 'Hi'])
+def greet(message):
+    bot.send_message(message.chat.id, "Hello")
+
+
+@bot.message_handler(commands=['licha'])
+def gasto_licha(message):
+    lista_mensaje = message.text.split(' ')
+    gastos_licha.append(int(lista_mensaje[-1]))
+    _deudor = deudor(gastos_licha, gastos_juli)
+    _monto = abs(sum(gastos_licha) - sum(gastos_juli))
+    bot.send_message(message.chat.id, f"Deudor: {_deudor} - ${_monto}")
+    print(message.text)
+
+
+@bot.message_handler(commands=['juli'])
+def gasto_juli(message):
+    lista_mensaje = message.text.split(' ')
+    gastos_juli.append(int(lista_mensaje[-1]))
+    _deudor = deudor(gastos_licha, gastos_juli)
+    _monto = abs(sum(gastos_licha) - sum(gastos_juli))
+    bot.send_message(message.chat.id, f"Deudor: {_deudor} - ${_monto}")
+
+
+bot.polling()
