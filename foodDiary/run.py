2e2856e2f8607d152289326130d4652c4cd79c3c
LisandroFilloy <lisandrofilloy@gmail.com>
2021-11-30 00:51:45 -0300

    fix

--- foodDiary/run.py
+++ foodDiary/run.py
@@ -38,7 +38,7 @@ def supplement(message):
 
     connector = sqlite3.connect('food_diary_database.db')
     cursor = connector.cursor()
-    cursor.execute(f"""INSERT INTO foods VALUES ('{_date}', '{_description}')""")
+    cursor.execute(f"""INSERT INTO supplements VALUES ('{_date}', '{_description}')""")
     connector.commit()
     connector.close()
 
